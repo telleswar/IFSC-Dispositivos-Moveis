345f9860decd87b387d02b17e549c5ecd39224a9
telleswar <silva12341@gmail.com>
2023-03-09 21:44:04 -0300

    atualização

--- app/src/main/java/com/ifsc/meuapp/MainActivity.java
+++ app/src/main/java/com/ifsc/meuapp/MainActivity.java
@@ -1,11 +1,14 @@
 package com.ifsc.meuapp;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    Fragment fragmentA, fragmentB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
     public void abrirTela(View v){
         Intent i = new Intent(this, MainActivity2.class);
         startActivity(i);
+    }
+
+    public void abreFragmentA(View v){
+        fragmentA = new fragment_a();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.framemain, fragmentA);
+        transaction.commit();
+    }
+
+    public void abreFragmentB(View v){
+        fragmentB = new fragment_b();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.framemain, fragmentB);
+        transaction.commit();
     }
 }
